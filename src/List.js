d30768f076dcedb6948ea371537e2a42a6e23251
juliausanova <juliausanova@gmail.com>
2019-11-20 17:34:40 +0300

    Fix list

--- src/List.js
+++ src/List.js
@@ -31,9 +31,7 @@ export default class List extends Component {
             <Router>
                 <div className="List">
                     <ul className="List-body">
-                        <li className="List-item">
-                            {array.map(c => <Link to={`/${c.id}`}><ListItem name={c.id} id={c.name} /></Link>)}
-                        </li>
+                        {array.map(c => <li className="List-item"><Link to={`/${c.id}`}><ListItem name={c.id} id={c.name} /></Link></li>)}
                     </ul>
 
                     <Switch>
