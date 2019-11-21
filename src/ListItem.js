8e0e508f5f2701505dfba1a911c269297eb6da46
juliausanova <juliausanova@gmail.com>
2019-11-21 11:34:51 +0300

    update dependencies

--- src/ListItem.js
+++ src/ListItem.js
@@ -7,6 +7,9 @@ Amplify.configure(aws_exports);
 
 export default function ListItem(props) {
     return (
+        <div>
             <span>This is id: {props.id}, this is name: {props.name}, this is url: {props.url}</span>
+        </div>
+
     );
 }
